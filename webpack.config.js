6ad2c8ed2e8bcdef70703150c59c359092d9b99e
Sunil Poojari <sunilpoojaridev@gmail.com>
2024-03-15 11:36:44 +0530

    added prod script to bundle the project and start the application in prod mode

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: "./server.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
